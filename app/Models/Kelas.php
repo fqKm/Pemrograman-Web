a8b48d0617d5cfa45d5a12d3363f8c3fc865969f
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-09-29 14:04:26 +0700

    Update Kelas.php

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kelas extends Model
 {
-    //
+    protected $table = 'kelas';
+    protected $primaryKey = 'kelas_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id_pelatih',
+        'class_name',
+        'start_time',
+        'end_time',
+        'max_capacity',
+        'description',
+    ];
+    public $timestamps = true;
+
+    // Relasi: Kelas dimiliki oleh satu Pelatih
+    public function pelatih()
+    {
+        return $this->belongsTo(Pelatih::class, 'id_pelatih', 'id');
+    }
 }
