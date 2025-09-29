9ac1fb37f538f70173b25c2fb6124571c9988bcd
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-09-29 14:04:45 +0700

    Update Pelatih.php

--- app/Models/Pelatih.php
+++ app/Models/Pelatih.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pelatih extends Model
 {
-    //
+    protected $table = 'pelatih';
+    protected $primaryKey = 'id_pelatih';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = ['nama_pelatih', 'spesialisasi', 'tanggal_masuk'];
+    public $timestamps = true;
 }
