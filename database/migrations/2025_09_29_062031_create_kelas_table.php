73dfd2cf666fd12fb17214d5ea4bdf1a397fb2e7
muzakkiahsan <115060485+muzakkiahsan@users.noreply.github.com>
2025-09-29 14:07:02 +0700

    Update Kelas

--- database/migrations/2025_09_29_062031_create_kelas_table.php
+++ database/migrations/2025_09_29_062031_create_kelas_table.php
@@ -11,17 +11,27 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kelas', function (Blueprint $table) {
-            $table->id();
+            Schema::create('kelas', function (Blueprint $table) {
+            $table->id();           // PK
+
+            // Kolom-kolom dari ERD
+            $table->string('class_name', 100);
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->unsignedInteger('max_capacity')->default(0);
+            $table->text('description')->nullable();
             $table->timestamps();
+
+            // Constraint FK
+            $table->foreign('trainer_id')
+                  ->onUpdate('cascade')
+                  ->onDelete('restrict');   // atau ->onDelete('cascade') sesuai kebutuhan
         });
     }
-
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('kelas');
     }
 };
