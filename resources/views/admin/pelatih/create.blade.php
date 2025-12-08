55da85fca6e20cae68153a03cc89b19cab3a1b18
muzakkiahsan <muzakkiahsan@gmail.com>
2025-12-08 22:12:34 +0700

    coba merge kuda

--- resources/views/admin/pelatih/create.blade.php
+++ resources/views/admin/pelatih/create.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Tambah Pelatih Baru') }}</h2>
     </x-slot>
     <div class="py-12">
+        <h2>kuda</h2>
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
